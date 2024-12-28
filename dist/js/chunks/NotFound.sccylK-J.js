a045f815b67fccc3589966098b59b39d65988a55
jamelclyons <jamel.c.lyons@gmail.com>
2024-12-28 11:55:36 -0500

    Added /dist to .gitignore and updated github actions for env var.

--- dist/js/chunks/NotFound.sccylK-J.js
+++
@@ -1,2 +0,0 @@
-import{j as o}from"../index.js";const n=()=>o.jsx("section",{className:"not-found",children:o.jsx("h2",{children:"404 - Page Not Found"})});export{n as default};
-//# sourceMappingURL=NotFound.sccylK-J.js.map
--- dist/js/chunks/Resume.CeFaE-8Q.js
+++
@@ -1,2 +0,0 @@
-import{r,j as e}from"../index.js";const m=({user:s})=>(r.useEffect(()=>{document.title=`Resume - ${s?.name}`},[]),e.jsx("section",{className:"resume",children:e.jsx("main",{children:e.jsx("iframe",{id:"pdfViewer",src:"../../Resume.pdf"})})}));export{m as default};
-//# sourceMappingURL=Resume.CeFaE-8Q.js.map
