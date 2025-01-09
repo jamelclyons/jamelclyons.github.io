1c150a61e7dfe4cd516cc2607bf2e1ac9c9a6ba8
jamelclyons <jamel.c.lyons@gmail.com>
2025-01-09 09:43:21 -0500

    Refactoring 2025-01-09 @ 09:43:21

--- src/model/ProjectDetails.ts
+++ src/model/ProjectDetails.ts
@@ -1,22 +1,24 @@
 import Model from './Model';
 
 class ProjectDetails extends Model {
+  public: boolean = false;
+  clientID: string
   clientName: string;
   startDate: string;
   endDate: string;
   detailsList: Array<string>;
   teamList: Array<string>;
-  clientID: string
 
   constructor(data?: Record<string, any>) {
     super();
 
-    this.clientName = data?.client_name || '';
+    this.public = data?.public;
+    this.clientName = data?.client_name || 'J.C. LYONS ENTERPRISES LLC';
     this.startDate = data?.start_date || '';
     this.endDate = data?.end_date || '';
     this.detailsList = data?.details_list || [];
     this.teamList = data?.team_list || [];
-    this.clientID = data?.client_id || '';
+    this.clientID = data?.client_id || 0;
   }
 }
 
