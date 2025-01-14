f27140d66d38807c960377255299b98f9170d0f3
jamelclyons <jamel.c.lyons@gmail.com>
2025-01-14 01:04:23 -0500

    Integraded gulp config into vite

--- gulpfile.js
+++ gulpfile.js
@@ -1,26 +1,78 @@
-const gulp = require('gulp');
-const sass = require('gulp-sass')(require('sass'));
-const clean = require('gulp-clean');
-const concat = require('gulp-concat');
-const cleanCSS = require('gulp-clean-css');
+import gulp from 'gulp';
+import gulpSass from 'gulp-sass';
+import * as sass from 'sass';
+import concat from 'gulp-concat';
+import cleanCSS from 'gulp-clean-css';
+import hash from 'gulp-hash-filename';
+import { deleteAsync } from 'del';
+
+import path from 'path';
+import fs from 'fs';
+import { glob } from 'glob';
+
+const sassCompiler = gulpSass(sass);
 
 const paths = {
-  scss: './scss/**/*.scss',
-  css: './dist/css'
+    scss: './scss/**/*.scss',
+    css: './dist/css',
+    html: './index.html'
+};
+const regex = /dist\/css\/index-[a-f0-9]{32}\.css/;
+
+gulp.task('clean', async () => {
+    await deleteAsync([`${paths.css}`]);
+});
+
+const findCss = async () => {
+    const files = await glob(path.join(paths.css, '*.css'), (err, files) => {
+        if (err) {
+            console.error('Error searching for CSS files:', err);
+            return [];
+        }
+
+        return files;
+    });
+
+    return files;
 };
 
-gulp.task('clean', function () {
-    return gulp.src('./dist/css/**/*', { read: false, allowEmpty: true })
-        .pipe(clean());
-});
+const replaceCssInHtml = (cssFileName) => {
+
+    fs.readFile(paths.html, 'utf8', (err, data) => {
+        if (err) {
+            console.error('Error reading the HTML file:', err);
+            return;
+        }
+
+        const updatedHtml = data.replace(/href=".*?index(?:-[a-f0-9]{32})?\.css"/, `href="${cssFileName}"`);
+
+        fs.writeFile(paths.html, updatedHtml, 'utf8', (err) => {
+            if (err) {
+                console.error('Error writing to the HTML file:', err);
+            } else {
+                console.log('HTML file updated with new CSS file name:', cssFileName);
+            }
+        });
+    });
+}
 
 gulp.task('build', gulp.series('clean', function () {
     return gulp.src(paths.scss)
-        .pipe(sass({ outputStyle: 'expanded' })
-        .on('error', sass.logError))
+        .pipe(sassCompiler({ outputStyle: 'expanded' })
+            .on('error', sassCompiler.logError))
         .pipe(concat('index.css'))
         .pipe(cleanCSS())
-        .pipe(gulp.dest(paths.css));
+        .pipe(hash())
+        .pipe(gulp.dest(paths.css))
+        .on('end', function () {
+            findCss().then((files) => {
+                const cssFileName = files.filter(file => regex.test(file))[0];
+
+                if (cssFileName) {
+                    replaceCssInHtml(cssFileName);
+                }
+            });
+        });
 }));
 
 gulp.task('watch', function () {
--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,45 @@
-import { defineConfig } from 'vite';
+import { defineConfig, PluginOption } from 'vite';
 import react from '@vitejs/plugin-react';
 import rollupOptions from './rollup.config';
+
+import { exec } from 'child_process';
 
 import path from 'path';
 
 import typescript from '@rollup/plugin-typescript';
+
+const gulp = (): PluginOption => {
+  return {
+    name: 'run-gulp-tasks',
+    apply: 'serve',
+    configureServer(server) {
+      // This runs after Vite's dev server is fully initialized
+      server.httpServer?.once('listening', () => {
+        return new Promise<void>((resolve) => {
+          console.log('Starting Gulp watch...');
+          const gulpProcess = exec('gulp watch');
+
+          gulpProcess.stdout?.on('data', (data) => {
+            console.log(`[Gulp]: ${data}`);
+          });
+
+          gulpProcess.stderr?.on('data', (data) => {
+            console.error(`[Gulp Error]: ${data}`);
+          });
+
+          gulpProcess.on('close', (code) => {
+            if (code !== 0) {
+              console.error(`Gulp watch process exited with code ${code}`);
+            } else {
+              console.log('Gulp watch process completed successfully.');
+            }
+            resolve();
+          });
+        });
+      });
+    },
+  };
+};
 
 /** @type {import('vite').UserConfig} */
 export default defineConfig({
@@ -14,7 +49,7 @@ export default defineConfig({
     },
     extensions: ['.ts', '.tsx', '.js', '.jsx'],
   },
-  plugins: [react(), typescript({ tsconfig: './tsconfig.json' })],
+  plugins: [react(), typescript({ tsconfig: './tsconfig.json' }), gulp()],
   define: {
     'import.meta.env': process.env,
   },
