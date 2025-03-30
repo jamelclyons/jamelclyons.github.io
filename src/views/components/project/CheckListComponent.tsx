4a61a4da7439c08c13ba2546e32ca6e7fa105e8f
jamelclyons <jamel.c.lyons@gmail.com>
2025-03-30 18:28:52 -0400

    Refactoring 2025-03-30 @ 18:28:52

--- src/views/components/project/CheckListComponent.tsx
+++ src/views/components/project/CheckListComponent.tsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react';
 
-import Task from '../../../model/Task';
+import Task from '@/model/Task';
 import CheckList from '@/model/CheckList';
 
 interface CheckListProps {
@@ -13,33 +13,32 @@ const CheckListComponent: React.FC<CheckListProps> = ({ checkList }) => {
   const [tasks, setTasks] = useState<Set<Task>>(checkList.tasks);
 
   useEffect(() => { setId(checkList.id) }, [checkList.id, setId]);
-  useEffect(() => { setTitle(checkList.title) }, [checkList.id, setTitle]);
-  useEffect(() => { setTasks(checkList.tasks) }, [checkList.id, setId]);
+  useEffect(() => { setTitle(checkList.title) }, [checkList.title, setTitle]);
+  useEffect(() => { setTasks(checkList.tasks) }, [checkList.tasks, setId]);
 
   return (
     <>
-      {tasks ? (
+      {tasks.size > 0 ? (
         <div className="checklist" id={id}>
 
           {title && <h4>{title}</h4>}
 
-          {Array.isArray(tasks) &&
-            tasks.map((task, index) => (
-              <span className='task' key={index}>
-                <input
-                  type="checkbox"
-                  name={`task_${index}`}
-                  id={`task_${index}`}
-                  checked={task.status}
-                  disabled
-                />
-                {task.details ?
-                  (<a className='task-details' href={task.details} target="_blank">
-                    <h5>{task.description}</h5>
-                  </a>) :
-                  <h5>{task.description}</h5>}
-              </span>
-            ))}
+          {Array.from(tasks).map((task, index) => (
+            <span className='task' key={index}>
+              <input
+                type="checkbox"
+                name={`task_${index}`}
+                id={`task_${index}`}
+                checked={task.status}
+                disabled
+              />
+              {task.details ?
+                (<a className='task-details' href={task.details} target="_blank">
+                  <h5>{task.description}</h5>
+                </a>) :
+                <h5>{task.description}</h5>}
+            </span>
+          ))}
         </div>
       ) : (
         ''
